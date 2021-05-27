6a1a84fd4e6ca9f7a4c310fa2711bb6604256474
pmp93 <pedro.martinez@ts-iberia.com>
2021-05-27 20:57:01 +0200

    implementado capacitor a actual

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,18 +5,7 @@ const config: CapacitorConfig = {
   appName: 'comms-app',
   webDir: 'www',
   bundledWebRuntime: false,
-  cordova: {
-    preferences: {
-      AndroidPersistentFileLocation: 'Compatibility',
-      ScrollEnabled: 'false',
-      BackupWebStorage: 'none',
-      SplashMaintainAspectRatio: 'true',
-      FadeSplashScreenDuration: '300',
-      SplashShowOnlyFirstTime: 'false',
-      SplashScreen: 'screen',
-      SplashScreenDelay: '3000'
-    }
-  }
+  
 };
 
 export default config;
